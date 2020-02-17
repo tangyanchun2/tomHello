1de83bdbefb0c8548c5f8524f08d268b04b576f0
tangyanchun2 <tangyanchun2@hotmail.com>
2020-02-17 22:20:44 +0800

    添加项目文件。

---
+++ GitTest2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test 2");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
